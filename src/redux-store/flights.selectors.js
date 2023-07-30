e5b2685143f7e57f1b6ba176f5bd2a8e346a9ce7
Kosanyak19 <kosvik264@gmail.com>
2023-07-30 18:36:39 +0300

    remove not necessary code

--- src/redux-store/flights.selectors.js
+++ src/redux-store/flights.selectors.js
@@ -1,18 +1,6 @@
-import { createSelector } from '@reduxjs/toolkit';
 import dayjs from 'dayjs';
 
 export const dateSelector = state => dayjs(new Date(state.flights.date));
 export const flightsListSelector = state => state.flights.flightsList;
 export const isPendingSelector = state => state.flights.pending;
 export const isDateCheckedSelector = state => state.flights.dateCheck;
-
-export const sortedFlightsListSelector = createSelector([flightsListSelector], flightsList => {
-  return flightsList.toSorted((a, b) => {
-    let dateA = a.departureDateExpected === 0 ? a.departureDate : a.departureDateExpected;
-    let dateB = b.departureDateExpected === 0 ? b.departureDate : b.departureDateExpected;
-    return (
-      new Date(dayjs(dateB, 'DD-MM-YYYY HH:mm:ss').format()).getTime() -
-      new Date(dayjs(dateA, 'DD-MM-YYYY HH:mm:ss').format()).getTime()
-    );
-  });
-});
